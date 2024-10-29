f8a043542c64e324b5b7bc44ab407a6438752956
MelindaSulaeman <melindasulaeman08@gmail.com>
2024-10-29 19:41:11 +0700

    Video 9 - Eloquent ORM & Post Model | Laravel 11

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -1,40 +1,12 @@
-<?php  
+<?php
 
 namespace App\Models;
 
-use Illuminate\Support\Arr;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-class Post {
-    public static function all() {
-        return [
-            [
-                'id' => 1,
-                'slug' => 'artikel-1',
-                'title' => 'Artikel 1',
-                'author' => 'Melinda Sulaeman',
-                'body' => ' Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vel optio, neque harum tempora quia ad,
-                enim alias unde exercitationem doloribus magni ab explicabo reprehenderit repellendus dolore ipsam nesciunt obcaecati quisquam.'
-            ],
-            [
-                'id' => 2,
-                'slug' => 'artikel-2',
-                'title' => 'Artikel 2',
-                'author' => 'Melinda Sulaeman',
-                'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed assumenda amet dolorem quos nisi, provident 
-                laudantium itaque odio delectus quisquam esse fuga natus quibusdam voluptate harum neque voluptatem. Ipsum, quos.'
-            ]
-        ];
-    }
-
-    public static function find($slug): array 
-    {
-
-        $post = Arr::first(static::all(), fn ($post) => $post ['slug'] == $slug );
-
-        if(!$post){
-            abort(404);
-        }
-
-        return $post;
-    }
+class Post extends Model
+{
+    Use HasFactory;
+    protected $fillable = ['title', 'author', 'slug', 'body'];
 }
